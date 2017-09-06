1900d02413638f5ad21c8f8e438f72c710e2d7c3
Olivier Mauras <olivier@mauras.ch>
2017-09-06 15:07:18 +0200

    Improve installer for better PyPI page

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
+from codecs import open
+import pypandoc
+import os
+
+# MD to RST automated convertion - Requires pandoc installed
+readme = pypandoc.convert('README.md', 'rst')
+f = open('README.rst','w+')
+f.write(readme)
+f.close()
+
+# Read README.rst for long description
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='dns-blackhole',
-    version='0.3',
+    version='0.8',
     description='A generic DNS black hole zone generator',
-    long_description='Let you fetch several known host list to generate a blackhole zone in the format of your choice',
-    url='http://git.mauras.ch/Various/dns-blackhole',
+    long_description=long_description,
+    url='https://github.com/coredumb/dns-blackhole',
     author='Olivier Mauras',
     author_email='olivier@mauras.ch',
     license='MIT',
@@ -17,5 +33,15 @@ setup(
                                         'etc/blacklist'])],
     entry_points = {
         'console_scripts': ['dns-blackhole=dnsblackhole.cli:main'],
-    }
+    },
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: System Administrators',
+    'Topic :: Internet :: Name Service (DNS)',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    ],
 )
